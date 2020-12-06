ab9f4e541bc94a845da49d14b7373c78fbe603d9
ZeeshanFaisal10 <zeshanfaisal10@gmail.com>
2020-12-06 21:31:41 +0500

    Initial commit

---
+++ android/app/src/main/kotlin/com/flutter_sharedprefs_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.flutter_sharedprefs_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/SplashScreen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          child: Text(
+              "This is Splash Screen"
+          ),
+        ),
+      ),
+    );
+  }
+}
